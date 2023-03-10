570debba29fe46b6eb68db2879205341ec1b7e06
Yewande Olawoyin <yewandec@gmail.com>
2023-03-10 10:27:18 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Print single digit numbers
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	for (i = 0; 1 < 10; 1++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
